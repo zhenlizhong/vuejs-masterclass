ff0f428de9a33966411b4b0dde1ff72d3c875134
zen <zhenli.zhong0314@gmail.com>
2025-03-31 17:16:18 +0100

    Create Dynamic Indvidual Project page and Task Page

--- database/types.ts
+++ database/types.ts
@@ -1,191 +1,194 @@
 export type Json =
-    | string
-    | number
-    | boolean
-    | null
-    | { [key: string]: Json | undefined }
-    | Json[]
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
-    public: {
-        Tables: {
-            projects: {
-                Row: {
-                    collaborators: string[]
-                    created_at: string
-                    id: number
-                    name: string
-                    slug: string
-                    status: Database["public"]["Enums"]["current_status"]
-                }
-                Insert: {
-                    collaborators?: string[]
-                    created_at?: string
-                    id?: never
-                    name: string
-                    slug: string
-                    status?: Database["public"]["Enums"]["current_status"]
-                }
-                Update: {
-                    collaborators?: string[]
-                    created_at?: string
-                    id?: never
-                    name?: string
-                    slug?: string
-                    status?: Database["public"]["Enums"]["current_status"]
-                }
-                Relationships: []
-            }
-            tasks: {
-                Row: {
-                    collaborators: string[]
-                    created_at: string
-                    description: string
-                    due_date: string | null
-                    id: number
-                    name: string
-                    project_id: number | null
-                    status: Database["public"]["Enums"]["current_status"]
-                }
-                Insert: {
-                    collaborators?: string[]
-                    created_at?: string
-                    description: string
-                    due_date?: string | null
-                    id?: never
-                    name: string
-                    project_id?: number | null
-                    status?: Database["public"]["Enums"]["current_status"]
-                }
-                Update: {
-                    collaborators?: string[]
-                    created_at?: string
-                    description?: string
-                    due_date?: string | null
-                    id?: never
-                    name?: string
-                    project_id?: number | null
-                    status?: Database["public"]["Enums"]["current_status"]
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "tasks_project_id_fkey"
-                        columns: ["project_id"]
-                        isOneToOne: false
-                        referencedRelation: "projects"
-                        referencedColumns: ["id"]
-                    },
-                ]
-            }
+  public: {
+    Tables: {
+      projects: {
+        Row: {
+          collaborators: string[]
+          created_at: string
+          description: string | null
+          id: number
+          name: string
+          slug: string
+          status: Database["public"]["Enums"]["current_status"]
         }
-        Views: {
-            [_ in never]: never
+        Insert: {
+          collaborators?: string[]
+          created_at?: string
+          description?: string | null
+          id?: never
+          name: string
+          slug: string
+          status?: Database["public"]["Enums"]["current_status"]
         }
-        Functions: {
-            [_ in never]: never
+        Update: {
+          collaborators?: string[]
+          created_at?: string
+          description?: string | null
+          id?: never
+          name?: string
+          slug?: string
+          status?: Database["public"]["Enums"]["current_status"]
         }
-        Enums: {
-            current_status: "in-progress" | "completed"
+        Relationships: []
+      }
+      tasks: {
+        Row: {
+          collaborators: string[]
+          created_at: string
+          description: string
+          due_date: string | null
+          id: number
+          name: string
+          project_id: number | null
+          status: Database["public"]["Enums"]["current_status"]
         }
-        CompositeTypes: {
-            [_ in never]: never
+        Insert: {
+          collaborators?: string[]
+          created_at?: string
+          description: string
+          due_date?: string | null
+          id?: never
+          name: string
+          project_id?: number | null
+          status?: Database["public"]["Enums"]["current_status"]
         }
+        Update: {
+          collaborators?: string[]
+          created_at?: string
+          description?: string
+          due_date?: string | null
+          id?: never
+          name?: string
+          project_id?: number | null
+          status?: Database["public"]["Enums"]["current_status"]
+        }
+        Relationships: [
+          {
+            foreignKeyName: "tasks_project_id_fkey"
+            columns: ["project_id"]
+            isOneToOne: false
+            referencedRelation: "projects"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
     }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      current_status: "in-progress" | "completed"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
 }
 
 type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-    PublicTableNameOrOptions extends
+  PublicTableNameOrOptions extends
     | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-            Row: infer R
-        }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
     ? R
     : never
-    : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-            Row: infer R
-        }
-    ? R
-    : never
+        Row: infer R
+      }
+      ? R
+      : never
     : never
 
 export type TablesInsert<
-    PublicTableNameOrOptions extends
+  PublicTableNameOrOptions extends
     | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-        Insert: infer I
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
     }
     ? I
     : never
-    : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
         Insert: infer I
-    }
-    ? I
-    : never
+      }
+      ? I
+      : never
     : never
 
 export type TablesUpdate<
-    PublicTableNameOrOptions extends
+  PublicTableNameOrOptions extends
     | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-        Update: infer U
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
     }
     ? U
     : never
-    : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
         Update: infer U
-    }
-    ? U
-    : never
+      }
+      ? U
+      : never
     : never
 
 export type Enums<
-    PublicEnumNameOrOptions extends
+  PublicEnumNameOrOptions extends
     | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
-    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-    : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
     : never
 
 export type CompositeTypes<
-    PublicCompositeTypeNameOrOptions extends
+  PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
-    CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-        schema: keyof Database
-    }
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-    : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -13,3 +13,36 @@ export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 export const projectsQuery = supabase.from('projects').select()
 export type Projects = QueryData<typeof projectsQuery>
+
+export const projectQuery = (slug: string) =>
+    supabase
+        .from('projects')
+        .select(`
+                *,
+                tasks (
+                    id,
+                    name,
+                    status,
+                    due_date
+                )
+        `)
+        .eq('slug', slug)
+        .single()
+
+export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: string) =>
+    supabase
+        .from('tasks')
+        .select(`
+                *,
+                projects (
+                    id,
+                    name,
+                    slug
+                    )
+        `)
+        .eq('id', Number(id))
+        .single()
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
