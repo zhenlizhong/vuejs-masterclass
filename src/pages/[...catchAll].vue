3936cee4e55bc18c30be09485e52220f90ccdee5
zen <zhenli.zhong0314@gmail.com>
2025-03-31 17:47:43 +0100

    Adjust the Error Page for Custom Errors

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
 
-useErrorStore().setError()
+useErrorStore().setError({ error: 'Page not found, Who are you??', customCode: 404 })
 </script>
 
 <template>
-  <AppErrorPage />
+  <div></div>
 </template>
