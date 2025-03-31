450691f77ed05016e2e04f0db6a395f0ce6c4980
zen <zhenli.zhong0314@gmail.com>
2025-03-31 19:37:00 +0100

    using v-model for the register form

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,4 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+const formData = ref({
+  username: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+})
+
+const signup = async () => {
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.value.email,
+    password: formData.value.password,
+  })
+
+  if (error) return console.log(error)
+
+  console.log(data)
+}
+</script>
 
 <template>
   <div
@@ -14,29 +36,60 @@
           <Button variant="outline" class="w-full"> Register with Google </Button>
           <Separator label="Or" />
         </div>
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="signup">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
-            <Input id="username" type="text" placeholder="johndoe19" required />
+            <Input
+              id="username"
+              type="text"
+              placeholder="johndoe19"
+              required
+              v-model="formData.username"
+            />
           </div>
           <div class="flex flex-col sm:flex-row justify-between gap-4">
             <div class="grid gap-2">
               <Label id="first_name" class="text-left">First Name</Label>
-              <Input id="first_name" type="text" placeholder="John" required />
+              <Input
+                id="first_name"
+                type="text"
+                placeholder="John"
+                required
+                v-model="formData.firstName"
+              />
             </div>
             <div class="grid gap-2">
               <Label id="last_name" class="text-left">Last Name</Label>
-              <Input id="last_name" type="text" placeholder="Doe" required />
+              <Input
+                id="last_name"
+                type="text"
+                placeholder="Doe"
+                required
+                v-model="formData.lastName"
+              />
             </div>
           </div>
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input id="email" type="email" placeholder="johndoe19@example.com" required />
+            <Input
+              id="email"
+              type="email"
+              placeholder="johndoe19@example.com"
+              required
+              v-model="formData.email"
+            />
           </div>
 
           <div class="grid gap-2">
             <Label id="password" class="text-left">Password</Label>
-            <Input id="password" type="password" placeholder="*****" autocomplete required />
+            <Input
+              id="password"
+              type="password"
+              placeholder="*****"
+              autocomplete
+              required
+              v-model="formData.password"
+            />
           </div>
 
           <div class="grid gap-2">
@@ -47,6 +100,7 @@
               placeholder="*****"
               autocomplete
               required
+              v-model="formData.confirmPassword"
             />
           </div>
           <Button type="submit" class="w-full"> Register </Button>
